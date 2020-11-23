47dd5e1d0b9829265a5f8f4bae876ed4941c1d23
MiguelSWolf <soulmiguel@gmail.com>
2020-11-23 10:58:21 -0300

    função simples de consumo do youtube
    função ainda estática, sem passar infos para o outro componente

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -7,7 +7,8 @@
           <input
             type="text"
             class="input is-large"
-            placeholder="Search for your favority band..."
+            v-model="searchField"
+            placeholder="Search for your favority..."
           />
           <input type="submit" value="search" class="button is-large" />
         </form>
@@ -20,10 +21,38 @@
 <script>
 export default {
   name: "Home",
+  data() {
+    return {
+      searchField: "Dua Lipa",
+      baseYoutubeAPI: "https://youtube.googleapis.com/youtube/v3/"
+    };
+  },
   methods: {
     onSubmit: function() {
-      console.log("buscou");
-      this.$router.push({ name: "band-page" });
+      console.log("buscou por " + this.searchField);
+      this.searchYoutube();
+      // this.$router.push({ name: "band-page" });
+    },
+    searchYoutube: function() {
+      console.log("busca youtube começou....");
+      const url = `${this.baseYoutubeAPI}search`;
+      const params = {
+        part: "snippet",
+        order: "viewCount",
+        q: this.searchField,
+        type: "video",
+        videoDefinition: "high",
+        key: process.env.VUE_APP_GOOGLE_API
+      };
+      this.$http
+        .get(url, { params })
+        .then(resource => {
+          console.log({ resource });
+        })
+        .catch(error => {
+          console.error("some error");
+          console.error({ error });
+        });
     }
   }
 };
