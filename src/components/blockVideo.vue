628a337cbb2f41a119052d5e8a2c6729365d94d3
MiguelSWolf <soulmiguel@gmail.com>
2020-11-23 07:51:20 -0300

    Inicio da estilização do bloco de videos

--- src/components/blockVideo.vue
+++ src/components/blockVideo.vue
@@ -1,12 +1,12 @@
 <template>
   <article class="video">
-    <figure>
+    <figure class="video-thumbnail">
       <img :src="video.thumbnail.medium.url" :alt="altDescription" />
     </figure>
     <div class="context">
       <h2 class="video-title">{{ video.title }}</h2>
       <h3 class="video-channel">{{ video.channelTitle }}</h3>
-      <div class="published">{{ video.publishedAt }}</div>
+      <div class="video-published">{{ publishedDate }}</div>
       <!-- <div class="description">{{ video.description }}</div> -->
     </div>
     <!-- {{ video.videoId }} -->
@@ -19,6 +19,14 @@ export default {
   computed: {
     altDescription: function() {
       return `Image from youtube video - ${this.video.title}`;
+    },
+    publishedDate: function() {
+      const options = { year: "numeric", month: "long", day: "numeric" };
+      const dateStr = new Date(this.video.publishedAt).toLocaleDateString(
+        "en-US",
+        options
+      );
+      return `Published on ${dateStr}`;
     }
   }
 };
