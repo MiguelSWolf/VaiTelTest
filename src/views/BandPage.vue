b628d972829592272206f62b3ec45f1687307c7d
MiguelSWolf <soulmiguel@gmail.com>
2020-11-24 06:29:53 -0300

    Removendo dados estáticos
    Removendo dados estáticos da pagina de bandas para consumir dados do retorno das APIs

--- src/views/BandPage.vue
+++ src/views/BandPage.vue
@@ -2,9 +2,9 @@
   <div class="band">
     <header>
       <div class="container">
-        <h2 class="name">{{ name }}</h2>
+        <h2 class="name">{{ band.name }}</h2>
         <figure class="figure">
-          <img :src="image" />
+          <img :src="band.image" />
         </figure>
       </div>
     </header>
@@ -13,7 +13,7 @@
         <nav class="social">
           <h2>External Links</h2>
           <icon-social
-            v-for="link in externalLinks"
+            v-for="link in band.externalLinks"
             :key="link.type"
             :social="link"
           />
@@ -22,7 +22,7 @@
         </nav>
         <section class="videos">
           <block-video
-            v-for="video in videos"
+            v-for="video in band.videos"
             :key="video.videoId"
             :video="video"
           />
@@ -33,7 +33,6 @@
 </template>
 
 <script>
-import DataGuns from "@/assets/test-data/youtube-guns.json";
 import blockVideo from "@/components/blockVideo";
 import iconSocial from "@/components/iconSocial";
 export default {
@@ -41,62 +40,10 @@ export default {
     blockVideo,
     iconSocial
   },
-  data() {
-    return {
-      name: "Guns n' roses",
-      // background: "https://s1.ticketm.net/dam/a/f01/ad68066e-5401-42bb-ba79-737ca8d65f01_1352251_TABLET_LANDSCAPE_16_9.jpg",
-      image:
-        "https://s1.ticketm.net/dam/a/f01/ad68066e-5401-42bb-ba79-737ca8d65f01_1352251_RETINA_PORTRAIT_3_2.jpg",
-      externalLinks: [
-        {
-          type: "youtube",
-          href: "https://www.youtube.com/user/GunsNRosesVEVO"
-        },
-        {
-          type: "twitter",
-          href: "https://twitter.com/gunsnroses"
-        },
-        {
-          type: "itunes",
-          href: "https://itunes.apple.com/artist/id106621"
-        },
-        {
-          type: "lastfm",
-          href: "http://www.last.fm/music/Guns+N'+Roses"
-        },
-        {
-          type: "wiki",
-          href: "https://en.wikipedia.org/wiki/Guns_N'_Roses"
-        },
-        {
-          type: "facebook",
-          href: "https://www.facebook.com/gunsnroses"
-        },
-        {
-          type: "instagram",
-          href: "https://www.instagram.com/gunsnroses"
-        },
-        {
-          type: "homepage",
-          href: "http://www.gunsnroses.com/"
-        }
-      ],
-      videos: []
-    };
-  },
-  mounted() {
-    DataGuns.items.forEach(video => {
-      const objVideo = {
-        videoId: video.id.videoId,
-        publishedAt: video.snippet.publishedAt,
-        title: video.snippet.title,
-        description: video.snippet.description,
-        thumbnail: video.snippet.thumbnails,
-        channelTitle: video.snippet.channelTitle,
-        channelId: video.snippet.channelId
-      };
-      this.videos.push(objVideo);
-    });
+  computed: {
+    band: function() {
+      return this.$store.state.band;
+    }
   }
 };
 </script>
