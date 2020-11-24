7a1e1b8830c8e87591c40c1cfc10d13e48e36ee1
MiguelSWolf <soulmiguel@gmail.com>
2020-11-24 06:27:36 -0300

    Adicionado funções para selecionar banda desejada
    Adicionada função para puxar dados do youtube e a banda selecionada da busca

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import VueResource from "vue-resource";
+import router from "../router/index";
 
 Vue.use(VueResource);
 Vue.use(Vuex);
@@ -39,12 +40,28 @@ export default new Vuex.Store({
       Vue.http
         .get(url, { params })
         .then(resource => {
+          state.band.videos = [];
           console.log({ videos: resource.body.items });
+          resource.body.items.forEach(video => {
+            this.commit("addVideo", { payload: { ...video } });
+          });
         })
         .catch(error => {
           console.error("some error");
           console.error({ error });
         });
+    },
+    addVideo(state, { payload }) {
+      const objVideo = {
+        videoId: payload.id.videoId,
+        publishedAt: payload.snippet.publishedAt,
+        title: payload.snippet.title,
+        description: payload.snippet.description,
+        thumbnail: payload.snippet.thumbnails,
+        channelTitle: payload.snippet.channelTitle,
+        channelId: payload.snippet.channelId
+      };
+      state.band.videos.push(objVideo);
     },
     searchTicketMaster(state) {
       console.log("busca ticketMaster começou....");
@@ -60,16 +77,39 @@ export default new Vuex.Store({
         .then(resource => {
           state.resultsTicketMaster = resource.body._embedded.attractions;
           console.log({ resource });
+          router.push({ name: "results-page" });
         })
         .catch(error => {
           console.error("some error");
           console.error({ error });
         });
+    },
+    selectedBand(state, { payload }) {
+      const selected = state.resultsTicketMaster.find(band => {
+        return band.id == payload.id;
+      });
+      console.log({ selected });
+      state.band.name = selected.name;
+      state.band.image = selected.images[1].url;
+      for (const type in selected.externalLinks) {
+        const url = selected.externalLinks[type][0].url;
+        const link = {
+          href: url,
+          type: type
+        };
+        console.log({ link });
+        state.band.externalLinks.push(link);
+      }
+      this.commit("searchYoutube", { payload: { bandName: selected.name } });
+      router.push({ name: "band-page" });
     }
   },
   actions: {
     searchBand(context, payload) {
       context.commit("searchBand", { payload });
+    },
+    selectedBand(context, payload) {
+      context.commit("selectedBand", { payload });
     }
   }
 });
